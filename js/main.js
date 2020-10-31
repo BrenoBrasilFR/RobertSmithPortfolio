7bf3b1bb667a40910dc364d3628736fa07199e0b
Breno Brasil <breno2019222@gmail.com>
2020-10-31 16:41:19 -0300

    Toggle Menu Finished

--- js/main.js
+++ js/main.js
@@ -3,19 +3,19 @@ const header  = document.getElementById("header");
 const items = document.querySelectorAll(".show");
 const image = document.querySelector("#showcase");
 const mainItem = document.querySelector(".showOnLoad");
-const menuToggle = document.querySelector(".menuToggle1");
-const menu = document.querySelector(".menuToggle");
-let menuBtnStatus = false;
+var x = window.matchMedia("(max-width: 736px)")
 
 window.onscroll = function () {
     if (window.pageYOffset > 50) {
         header.style.background = "black";
         header.style.boxShadow = "0px 0px 20px black";
+    } else if (x.matches) {
+        header.style.background = "black";
     } else {
         header.style.background = "transparent";
         header.style.boxShadow = "none";
-
     }
+    
 }
 
 /* SHOW ITEMS */
@@ -45,14 +45,34 @@ function myFunction() {
 }
 
 /* MENU */
+const menu = document.querySelector(".menuToggle");
+const menuTextCurrent = document.getElementById("current");
+const menuToggle = document.querySelector(".menuToggle1");
+const menuText = document.querySelectorAll(".a");
+const header0 = document.querySelector("#header");
+let menuBtnStatus = true;
+
 function toggleMenu(e) {
     e.preventDefault();
+    var i;
 
     if (menuBtnStatus == false) {
-        menuToggle.style.marginLeft = "0px";
+        for (i = 0; i < menuText.length; i++) {
+            menuText[i].style.color = "transparent";
+        }
+        menuToggle.style.marginTop = "0px";
+        menuToggle.style.backgroundColor = "transparent";
+        menuTextCurrent.style.borderBottom = "transparent solid 2px";
+        header0.style.borderBottom = "transparent solid 1px";
         menuBtnStatus = true;
     } else if (menuBtnStatus = true) {
-        menuToggle.style.marginLeft = "736px";
+        for (i = 0; i < menuText.length; i++) {
+            menuText[i].style.color = "white";
+        }
+        menuToggle.style.marginTop = "80px";
+        menuToggle.style.backgroundColor = "black";
+        menuTextCurrent.style.borderBottom = "lightgray solid 2px";
+        header0.style.borderBottom = "lightgray solid 1px";
         menuBtnStatus = false;
     }
 }
